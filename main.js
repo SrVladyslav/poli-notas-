f5fb53b12936672bfbad43aba6a7c540129c7c67
SrVladyslav <vlama@inf.upv.es>
2019-12-30 00:26:21 +0100

    Recap

--- main.js
+++
@@ -1,8 +0,0 @@
-var Parser = require('expr-eval').Parser;
-
-
-module.export = function getParsedFormula(formula) {
-	let pars = new Parser();
-	var expr = pars.parse(formula);
-	return expr.symbols();
-}
