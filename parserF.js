c5cdb61aaf7cf5a869cb5408cab79bc3d04e760a
SrVladyslav <vlama@inf.upv.es>
2019-12-30 23:32:27 +0100

    F

--- parserF.js
+++ parserF.js
@@ -73,17 +73,58 @@ function getExamsFromFormula(formula){
 		}
 	})
 	f = aux.filter((v, i, a) => a.indexOf(v) === i)
-	console.log(f)
+	///console.log(f)
 	return f
 }
 
-getExamsFromFormula('P1+(P2+(5-P2)/3*P3)+P4')
+///getExamsFromFormula('P1+(P2+(5-P2)/3*P3)+P4')
 
-//operators
+//gives the parentesis indexes
+function calculateParentesis(formula) {
+	var leftParentesis = []
+	var rigthParentesis = []
+	console.log(formula)
+	//looking for parentesis indexes
+	for(i = 0; i < formula.length; i++){
+		if(formula[i] == '('){
+			leftParentesis.push(i)
+		}else if(formula[i] == ')'){
+			rigthParentesis.push(i)
+		}
+	}
+	//solving parentesis
+	for(i = 0; i < leftParentesis.length; i ++){
 
-//calculates the value from formula
-function calculateFormula(formula, ...vars) {
-		
-} 
+	}
 
-calculateFormula('P1+(P2+(5-P2)/3*P3)+P4', 1,2,3,4,5)
+	console.log(leftParentesis, rigthParentesis)
+}
+
+//calculates the value from formula changing the parameters by given values ot them
+function calculateFormula(formula, vars) {
+	//obtaining parameters
+	var params = getExamsFromFormula(formula)
+	console.log("Params: " +params)
+	console.log("Vars: "+vars)
+	//replacing formula  by values
+	/*for(let i = 0; i < vars.length; i++){
+		console.log(formula.search(''+params[i]))
+		while(formula.search(params[i]) >= 0){
+			if(isNaN(params[i]+'')){
+				params[i] = 0
+			}
+			formula = formula.replace(''+params[i], vars[i]) //replacing given parameters by his value
+			formula
+		}
+		console.log(formula)
+	}*/
+	for(let i = 0; i < params.length; i++){
+		formula = formula.split(params[i]).join(vars[i])
+	}
+	return formula
+}
+
+
+
+g=calculateFormula(' (P2+(5-P2)/3*P3)+P4', [1,1,1,1])
+console.log("Formula final: "+g)
